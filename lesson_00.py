d8b358a22f1d3a6adb6a79a233d574ab8d417223
Nahid Shovo <shovonahid37@gmail.com>
2024-09-16 10:50:09 -0400

    python exercises

---
+++ lesson_00.py
@@ -0,0 +1,46 @@
+
+#step 2
+print("nahid")
+
+#step 3
+favShow="Law & Order SVU"
+print(favShow)
+
+# step 4
+floatNum= float(2.3283823823823)
+integerNum= int(2)
+
+#step 5
+print(floatNum+integerNum)
+
+#step 6
+
+favFood= ["Tandoori Chicken","Chicken Over Rice","Chicken Biryani"]
+
+#step 8
+age = int(input("Enter your age: ")) 
+
+#step 7
+if age > 18:
+    print("You can get a driver's license.")
+elif age < 18:
+    print("You are too young for a driver's license.")
+else:
+    print("You are exactly 18, so you can get a driver's license.")
+
+#step 9
+name=input("Enter your name: ")
+print(name)
+
+#step 10
+for x in favFood:
+    print(x)
+
+#step 11
+
+i = 1
+while i < 1:
+  print(i)
+  i = 1
+
+#step 12
