66c13c83820e3e45b155e7ba4f8560e7725c27c1
Nahid Shovo <shovonahid37@gmail.com>
2024-09-30 10:36:25 -0400

    saves to a file

--- Projects/Todo_tracker.py
+++ Projects/Todo_tracker.py
@@ -1,50 +1,65 @@
-todos = ['earn money', 'buy a porsche', 'drive the porsche']
+#file to store todos
+FILENAME = "to-do.txt"
 
-while True:  # tarts infinite loop :)
-    # isplays current todos with numbers
+#load existing todos if the file exists, otherwise start with an empty list
+file = open(FILENAME, 'r') if 'to-do.txt' in open(FILENAME, 'a+').name else None
+
+if file:
+    todos = file.read().splitlines()  #reads each line as a todo item
+    file.close()
+else:
+    todos = []  #if the file doesn't exist, start with an empty list
+
+#main application loop
+while True:  #tarts infinite loop :)
+    #display current todos with numbers
     print("\n-------------------------------------------")
-    print("\nYour current todos are:")
+    print("\nyour current todos are:")
     for i in range(len(todos)):  #loops through todos
-        print(f"{i+1}. {todos[i]}") #prints each todo with its index
+        print(f"{i+1}. {todos[i]}")  #prints each todo with its index
     print("\n-------------------------------------------")
 
-    #displays menu options
-    print("\nChoose an option:")
-    print("1 - Add a todo")
-    print("2 - Remove a todo")
-    print("3 - Quit")  #option to quit the program if they want to
+    #display menu options
+    print("\nchoose an option:")
+    print("1 - add a todo")
+    print("2 - remove a todo")
+    print("3 - quit")  #option to quit the program
 
     print("\n-------------------------------------------")
-    #gets the user's choice
-    action = input("\nEnter an option (1/2/3): ").lower()
-    
+    #get the user's choice
+    action = input("\nenter an option (1/2/3): ").lower()
+
     if action not in ['1', '2', '3', 'add', 'remove', 'quit']:  #checks if choice is valid
-        print("\nInvalid input. Please enter an option (add/remove/quit) or (1/2/3).")
-        continue  #skips to next iteration of loop
+        print("\ninvalid input. please enter an option (add/remove/quit) or (1/2/3).")
+        continue  #skips to the next iteration of loop
 
     if action == '1' or action == 'add':  #if choice is 1 or add
         print("\n-------------------------------------------")
-        #adds a new todo
-        new_todo = input("\nWhat is your new to-do? ")
-        todos.append(new_todo)  #adds new todo to the list
+        #add a new todo
+        new_todo = input("\nwhat is your new to-do? ")
+        todos.append(new_todo)  #add new todo to the list
 
     elif action == '2' or action == 'remove':  #if choice is 2 or remove
         print("\n-------------------------------------------")
-        #removes an existing todo by its index
-        index_to_remove = input(f"\nWhich # todo would you like to remove (1-{len(todos)}): ")
+        #remove an existing todo by its index
+        index_to_remove = input(f"\nwhich # todo would you like to remove (1-{len(todos)}): ")
         
         if not index_to_remove.isdigit():  #checks if input is a digit
-            print("\nInvalid input. Please enter a number.")
-            continue  #skip to next iteration of loop
+            print("\ninvalid input. please enter a number.")
+            continue  #skip to the next iteration of loop
         
         index_to_remove = int(index_to_remove)  #converts input to integer
         
         if 1 <= index_to_remove <= len(todos):  #checks if index is within valid range
             removed_todo = todos.pop(index_to_remove - 1)  #removes todo from the list
-            print(f"\nRemoved: {removed_todo}")
+            print(f"\nremoved: {removed_todo}")
         else:
-            print("\nInvalid number. Please enter a valid index.")
+            print("\ninvalid number. please enter a valid index.")
 
     elif action == '3' or action == 'quit':  #if choice is 3 or quit
-        print("\nBye byE!!")
+        print("\nsaving your todos and exiting...")
+        #save the todos to the file
+        with open(FILENAME, 'w') as file:
+            for todo in todos:
+                file.write(todo + "\n")  #writes each todo on a new line
         break  #exits the loop and ends the program
